7759bd445f1284808dddb296bd6a04ccc45da51e
saxs-14 <mamagauphathu@gmail.com>
2026-04-30 23:26:20 +0800

    Update Main greeting on main branch

--- src/main/java/dict421/gitassignmentproject/Main.java
+++ src/main/java/dict421/gitassignmentproject/Main.java
@@ -10,6 +10,6 @@ package dict421.gitassignmentproject;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello NetBeans. Welcome to version control using Git");
+        System.out.println("Hello Phathu. Welcome to version control using Git");
     }
 }
