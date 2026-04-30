c69ada8ca78b029711bc948140b82672d99d440f
saxs-14 <mamagauphathu@gmail.com>
2026-04-30 23:04:33 +0800

    Add Login.java with login functionality

---
+++ src/main/java/dict421/gitassignmentproject/Login.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dict421.gitassignmentproject;
+
+/**
+ *
+ * @author mamag
+ */
+public class Login {
+    public void loginUser(String username) {
+        System.out.println("Welcome, " + username + "! You have successfully logged in.");
+    }
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.loginUser("TestUser");
+    }
+}
