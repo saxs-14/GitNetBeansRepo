a9a331895a547f58f99ff7a240734d050248c23e
saxs-14 <mamagauphathu@gmail.com>
2026-04-30 20:30:54 +0800

    Initial commit – Main Java class

---
+++ src/main/java/dict421/gitassignmentproject/GitAssignmentProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dict421.gitassignmentproject;
+
+/**
+ *
+ * @author mamag
+ */
+public class GitAssignmentProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
